299de00bab0c977738f87474596f49b2e45ee6ed
gwonhong <gwonhong.dev@gmail.com>
2023-03-12 04:27:27 +0900

    inject box inside lecture & get student info

--- popup/popup.js
+++ popup/popup.js
@@ -1,29 +1,20 @@
-// window.addEventListener('DOMContentLoaded', () => {
-//     const enableButton = document.getElementById("enableButton");
-//     enableButton.addEventListener("click", () => {
-//         chrome.tabs.query({active: true}, function(tabs) {
-//             chrome.tabs.sendMessage(tabs[0].id, {method: "enableChat"}, function(response) {
-//                 if(response.method == "enableChat") {
-//                     alert("Success: " + response.method);
-//                 };
-//             });
-//         });
-//     });
-// });
-
-
 window.addEventListener('DOMContentLoaded', () => {
     let enableButton = document.getElementById("enableButton");
     let resultText = document.getElementById("resultText");
     enableButton.addEventListener("click", async () => {
         const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
         const response = await chrome.tabs.sendMessage(tab.id, {method: "enableChat"});
-        if(response.method == "success") {
-            resultText.textContent = "success";
-        } else if(response.method == "fail") {
-            resultText.textContent = "fail";
-        } else {
-            resultText.textContent = "error";
+        if(response.method == "enableChat") {
+            if(response.status == "success") {
+                resultText.textContent = "success";
+            } else if(response.status == "fail") {
+                resultText.textContent = "fail";
+            } else {
+                resultText.textContent = "error";
+            }
+        } else if (response.method == "test") {
+            resultText.textContent = response.text;
         }
+
       });
 });
--- scripts/content.js
+++ scripts/content.js
@@ -1,13 +1,19 @@
 function enableChat() { //returns true if success, false if not
-    let player_area = document.getElementById("player-area");
-    // check if player_area is null
-    if(player_area != null) {
+    //get inside the iframes where the player exists
+    let tool_content = document.getElementById("tool_content");
+    let studentId = tool_content.contentDocument.getElementById("root").getAttribute("data-user_login");
+    let studentName = tool_content.contentDocument.getElementById("root").getAttribute("data-user_name");
+    console.log(studentId);
+    
+    let video_frame = tool_content.contentDocument.getElementsByClassName("xnvc-video-frame")[0];
+
+    if(video_frame != null) {
         const chat_box = document.createElement("div")
         chat_box.id = "chat-box";
         chat_box.style.border = "2px solid black";
         chat_box.style.width = "100px";
         chat_box.style.height = "100px";
-        player_area.appendChild(chat_box);
+        video_frame.contentDocument.body.appendChild(chat_box);
         return true;
     }else{
         return false;
@@ -19,9 +25,9 @@ chrome.runtime.onMessage.addListener(
         if(request.method == "enableChat") {
             const success = enableChat();
             if(success) {
-                sendResponse({method: "success"});
+                sendResponse({method: "enableChat", status: "success"});
             } else {
-                sendResponse({method: "fail"});
+                sendResponse({method: "enableChat", status: "fail"});
             }
         }
     }
